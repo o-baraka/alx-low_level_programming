c046c6db6e6b817814a50b6d0ea1f2ee3d9e908b
obwogo baraka <obwogobaraka@gmail.com>
2023-08-14 06:10:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A c program that prints a line to the standard error
  * Return 1 (Success)
  */
-int main(void)
+int main()
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
